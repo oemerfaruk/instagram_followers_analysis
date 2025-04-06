cdec6f9f2dc20245d8990c1c30edfc084850969d
Ömer Faruk Aydın <omerfarukaydin@seoem.co>
2025-04-06 15:52:02 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# instagram followers analysis
+find users who are in following but not in followers
+![gif](https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExa2J2eWJybnRnd2FudGZqMnB4OTNvZTZmcGd4OWJjZndnb3hnc201cyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/jUwpNzg9IcyrK/giphy.gif)
+
+You have to download your data from app. following.json and followers_1.json files
+```
+instagram_followers_analysis/
+├── data/                   # folder of data
+│   ├── followers_1.json        # 
+│   └── following.json          # 
+├── .gitignore
+├── app.py                  # python file for analysis
+└── README.md
+```
+
+
+## Apps
+You need one app for analysis. You can choose for your project
+
+### app.py
+This is python file for analysis.
+#### modules
+````py
+import json
---
+++ app.py
@@ -0,0 +1,32 @@
+import json
+
+# read JSON File
+with open("following.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+# collect all "value" (usernames) values from following.json
+following = []
+for item in data.get("relationships_following", []):
+    for string_data in item.get("string_list_data", []):
+        following.append(string_data.get("value"))
+
+
+# read JSON File
+with open("followers_1.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+# collect all "value" (usernames) values from followers_1.json
+followers = []
+for item in data:
+    for string_data in item.get("string_list_data", []):
+        followers.append(string_data.get("value"))
+
+
+# by comparing the two lists
+# find users who are in following but not in followers
+not_following_back = [user for user in following if user not in followers]
+
+# print the result
+print("Users you are following but not followed back:")
+for user in not_following_back:
+    print(user)
